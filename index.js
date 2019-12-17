52e0c143b361cfb3d139260179105a6cb450db66
arjunlondhey <56534369+arjunlondhey@users.noreply.github.com>
2019-12-17 14:13:03 +0530

    intToRoman and flatten exercise submission

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const {
     init
-  } = require('./Memoize/memoize');
-  
+} = require('./Memoize/memoize');
 
-init();  
+const {
+    convertIntToRoman
+} = require('./IntToRoman/intToRoman.js');
+
+const {
+    flatten
+} = require('./Flatten/flatten.js');
+//init();
+//convertIntToRoman(19);
+flatten([1, 2, [3, 4, [5, 6]]], 2);
