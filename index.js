f8717af24326adf7e818017a67cb947e3cba934b
arjunlondhey <56534369+arjunlondhey@users.noreply.github.com>
2019-12-26 18:15:52 +0530

    add updateInventory exercise solution

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ let {
 let {
     balancedBraces
 } = require('./BalancedBraces/balancedBraces.js');
+let {
+    updateInventory
+} = require('./UpdateInventory/updateInventory.js');
 
-console.log(balancedBraces('for(i=0;i<x[i];i++){if(x[i]<10){b++;}else{b+=10;}'));
+console.log(updateInventory([
+    [21, 'Bowling Ball'],
+    [2, 'Dirty Sock'],
+    [1, 'Hair Pin'],
+    [5, 'Microphone'],
+], [
+    [2, 'Hair Pin'],
+    [3, 'Half-Eaten Apple'],
+    [67, 'Bowling Ball'],
+    [7, 'Toothpaste'],
+]));
